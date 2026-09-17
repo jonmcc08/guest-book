477241e2f7d243f468d51264747de46fa4b9e97b
Jonathan <jonathan.mccullagh@skola.taby.se>
2026-09-17 13:35:58 +0200

    Lagt till sparande information.

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template, redirect, request
+import json, os, time
+
+json_file = "users.json"
 
 app = Flask(__name__)
+
+def loadFile():
+    if not os.path.exists(json_file):
+        return []
+    try:
+        with open(json_file, encoding='utf-8') as file:
+            return json.load(file)
+    except Exception:
+        return []
 
 @app.route('/')
 def main():
@@ -13,8 +25,21 @@ def write():
 @app.route('/send', methods=["POST", "GET"])
 def send():
     information = request.form.to_dict()
+    currentFile = loadFile()
+    currentFile.append(information)
+    print(information)
+    all_info = True
     for part in information:
-        pass # FIXA SENARE
+        if information[part] == "":
+            all_info = False
+
+    if not all_info:
+        return "Missing information, please try again!"
+    
+
+    with open(json_file, 'w', encoding="utf-8") as file:
+        json.dump(currentFile, file, indent=4)
+
     return "Sent information"
 
 
